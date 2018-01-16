34b5d9ff55991357c4c3fe16a063bb2330dbf53b
bolino <adrien@adriencarpentier.com>
2018-01-16 20:55:23 +0100

    state for submission to Canard PC

--- play.js
+++ play.js
@@ -1,12 +1,15 @@
 var playState = {
 
 	preload: function() {
-		game.load.image('background', 'assets/background.png');
-		game.load.spritesheet('shadow_pigeon', 'assets/pigeons-shadows.png', 66, 50);
-		game.load.spritesheet('next_button', 'assets/next-button.png', 105, 76);
-		game.load.spritesheet('pigeon_booth', 'assets/pigeon-booth.png', 122, 121);
-		game.load.image('passport', 'assets/passport.png');
-		game.load.spritesheet('stamps', 'assets/stamps.png', 561, 150);
+		game.load.image('background', 'graphics/background.png');
+		game.load.spritesheet('shadow_pigeon', 'graphics/pigeons-shadows.png', 66, 50);
+		game.load.spritesheet('next_button', 'graphics/next-button.png', 105, 76);
+		game.load.spritesheet('pigeon_booth', 'graphics/pigeon-booth.png', 122, 121);
+		game.load.spritesheet('passport', 'graphics/passport.png', 481, 322);
+		game.load.spritesheet('stamp', 'graphics/stamps.png', 561, 150);
+		game.load.audio('stamp_audio', 'sounds/stamp.mp3');
+		game.load.audio('paper_audio', 'sounds/paper.mp3');
+		game.load.audio('flying_audio', 'sounds/flying.mp3');
 	},
 
 	create: function() {
@@ -19,110 +22,163 @@ var playState = {
 		pigeons = game.add.group();
 		var pigeon = [];
 		// here we'll create 12 of them evenly spaced apart
-		for (var i = 0; i < 10; i++) {
+		for (var i = 0; i < 9; i++) {
 			//  Create a pigeon sprite inside of the 'pigeons' group
-			pigeon[i] = pigeons.create(i*20, 20, 'shadow_pigeon');
-			//  Our two animations, walking left and right.
+			var random_nb = Math.floor((Math.random() * 16) - 8);
+			pigeon[i] = pigeons.create(i*20-10, 45+random_nb, 'shadow_pigeon');
 			pigeon[i].animations.add('flying', [1, 2]);
 		}
 
 		// NEXT BUTTON
 		next_button = game.add.sprite(100, 400, 'next_button');
 		next_button.animations.add('blink', [1, 2]);
-		next_button.play('blink', 4, true);
-		next_button.inputEnabled = true;
-		next_button.events.onInputDown.add(nextPigeon);
 
 		// STAMP
-		stamp = game.add.sprite(game.world.width-50, 200, 'stamps');
+		stamp = game.add.sprite(game.world.width-50, 200, 'stamp');
+		stamp.animations.add('green_action', [2, 0]);
+		stamp.animations.add('red_action', [1, 0]);
 		stamp.inputEnabled = true;
 		stamp.events.onInputDown.add(showStamp, this);
+		var stamp_audio = game.add.audio('stamp_audio');
+
+		// PASSPORT AUDIO
+		var paper_audio = game.add.audio('paper_audio');
+
+		// FLYING AUDIO
+		var flying_audio = game.add.audio('flying_audio');
+
 		function showStamp() {
-			console.log("show stamp");
-			stamp_show_tween = game.add.tween(stamp).to( { x: 439 }, 2000, "Quart.easeOut");
+			stamp_show_tween = game.add.tween(stamp).to( { x: 439 }, 1000, "Quart.easeOut");
 			stamp_show_tween.start();
 			stamp.events.onInputDown.removeAll();
-			stamp.events.onInputDown.add(hideStamp, this);
+			stamp.events.onInputDown.add(onStampClick, this);
+			function onStampClick() {
+				var hide_stamp_area = new Phaser.Rectangle(441,238,23,116);
+				var red_stamp_area = new Phaser.Rectangle(511,214,159,113);
+				var green_stamp_area = new Phaser.Rectangle(730,214,159,113);
+				var x = game.input.activePointer.positionDown.x;
+				var y = game.input.activePointer.positionDown.y;
+				// console.log(x,y); // TEST
+				if (red_stamp_area.contains(x, y)) {
+					stamp.play('red_action', 5, false);
+					stamp_audio.play();
+					if (typeof passport !== "undefined") {
+						passport.frame = 2;
+						accepted();
+					}
+				}
+				else if (green_stamp_area.contains(x, y)) {
+					stamp.play('green_action', 5, false);
+					stamp_audio.play();
+					if (typeof passport !== "undefined") {
+						passport.frame = 1;
+						//game.time.events.add(Phaser.Timer.SECOND * 1, accepted, this);
+						accepted();
+					}
+				}
+				else if (hide_stamp_area.contains(x, y)) {
+					hideStamp();
+				}
+			}
 		}
+
 		function hideStamp() {
-			console.log("hide stamp");
-			stamp_hide_tween = game.add.tween(stamp).to( { x: game.world.width-50 }, 2000, "Quart.easeOut");
+			stamp_hide_tween = game.add.tween(stamp).to( { x: game.world.width-50 }, 1000, "Quart.easeOut");
 			stamp_hide_tween.start();
 			stamp.events.onInputDown.removeAll();
 			stamp.events.onInputDown.add(showStamp, this);
 		}
-		
-		function nextPigeon() {
 
-			console.log("pigeon suivant");
 
-			// remove clickable state of next button
-			next_button.events.onInputDown.removeAll();
-			next_button.animations.stop(null, true);
-			next_button.frame = 0;
-
-			// PUT PIGEON IN BOOTH
-			pigeon_booth = game.add.sprite(0, 249, 'pigeon_booth');
-			pigeon_booth_tween = game.add.tween(pigeon_booth).to({x: 100, y: 249}, 1000, "Quart.easeOut");
-			pigeon_booth_tween.start();
-			pigeon_booth_tween.onComplete.add(passport);
-
-			// STAMPING
-			// TO DO
-			//
-			//
-
-			// STAMPED IS DONE
-			pigeon_booth_tween.onComplete.add(accepted);
-
-			function accepted() {
+		function accepted() {
+			hideStamp();
+			var fadeout_pigeon_booth_tween = game.add.tween(pigeon_booth).to( { alpha: 0 }, 300);
+			fadeout_pigeon_booth_tween.onComplete.add(changePigeonBooth);
+			fadeout_pigeon_booth_tween.start();
+			function changePigeonBooth() {
 				pigeon_booth.frame = 1;
-				//pigeon_booth.play('mad');
-				// pigeon flying
-				var flying_pigeon = game.add.sprite(30, 30, 'shadow_pigeon');
-				flying_pigeon.animations.add('flying', [1, 2]);
-				flying_pigeon.enableBody = true;
-				//flying_pigeon_tween = game.add.tween(flying_pigeon).to({x: 1000, y: 30}, 3000, "Quart.easeOut");
-				flying_pigeon.play('flying', 10, true);
-				//flying_pigeon.body.velocity.x = 200;
-				//game.physics.arcade.moveToXY(flying_pigeon, 126, 160, 200);
-				//flying_pigeon_tween.start();
-
+				var fadein_pigeon_booth_tween = game.add.tween(pigeon_booth).to( { alpha: 1 }, 300);
+				fadein_pigeon_booth_tween.onComplete.add(function(){
+					game.time.events.add(Phaser.Timer.SECOND * 1, pigeonDone, this); // wait for one second
+					function pigeonDone(){
+						// REMOVE PASSPORT
+						remove_passport_tween = game.add.tween(passport).to({x: 400, y: 600}, 1000, "Quart.easeOut");
+						paper_audio.play();
+						remove_passport_tween.start();
+						remove_passport_tween.onComplete.add(function(){
+							passport.destroy();
+						});
+						// REMOVE PIGEON FROM BOOTH
+						remove_pigeon_booth_tween = game.add.tween(pigeon_booth).to({x: -500, y: 300}, 1000, "Quart.easeOut");
+						remove_pigeon_booth_tween.start();
+						remove_pigeon_booth_tween.onComplete.add(function(){
+							// DESTROY PIGEON BOOTH
+							pigeon_booth.destroy();
+							// PIGEON FLYING
+							var flying_pigeon = game.add.sprite(90, 45, 'shadow_pigeon');
+							flying_pigeon.animations.add('flying', [1, 2]);
+							flying_audio.play();
+							var flying_pigeon_tween = game.add.tween(flying_pigeon).to( { x: 1000 }, 2000, 'Linear');
+							flying_pigeon.play('flying', 10, true);
+							flying_pigeon_tween.start();
+							flying_pigeon_tween.onComplete.add(function(){
+								flying_pigeon.destroy();
+								loop();
+							});
+						});
+					};
+				});
+				fadein_pigeon_booth_tween.start();
 			}
+		}
 
-			function passport() {
-				// PASSPORT
-				passport = game.add.sprite(200, 350, 'passport');
-				passport_tween = game.add.tween(passport).to({x: 350, y: 350}, 1000, "Quart.easeOut");
-				passport_tween.start();
-				// Enable input and allow for dragging of passport
-				passport.inputEnabled = true;
-				passport.input.enableDrag();
-				passport.events.onDragStart.add(onDragStart, this);
-				passport.events.onDragStop.add(onDragStop, this);
-				game.input.mouse.capture = true;
-				function onDown(sprite, pointer) {
-					result = "Down " + sprite.key;
-					console.log('down', sprite.key);
-				}
-				function onDragStart(sprite, pointer) {
-					//result = "Dragging " + sprite.key;
-				}
-				function onDragStop(sprite, pointer) {
-					if (pointer.y > 600) {
-						console.log('input disabled on', sprite.key);
-						sprite.input.enabled = false;
-						sprite.sendToBack();
+		function loop() {
+
+			next_button.play('blink', 4, true);
+			next_button.inputEnabled = true;
+			next_button.events.onInputDown.add(function(){
+
+				// NEXT PIGEON
+				// remove clickable state of next button
+				next_button.events.onInputDown.removeAll();
+				next_button.animations.stop(null, true);
+				next_button.frame = 0;
+
+				// PUT PIGEON IN BOOTH
+				pigeon_booth = game.add.sprite(0, 249, 'pigeon_booth');
+				pigeon_booth_tween = game.add.tween(pigeon_booth).to({x: 100, y: 249}, 1000, "Quart.easeOut");
+				pigeon_booth_tween.start();
+
+				// BRING PASSPORT
+				pigeon_booth_tween.onComplete.add(bringPassport);
+				function bringPassport() {
+					passport = game.add.sprite(0, 300, 'passport');
+					game.world.bringToTop(stamp); // stamps must stay on top
+					passport_tween = game.add.tween(passport).to({x: 400, y: 200}, 1000, "Quart.easeOut");
+					paper_audio.play();
+					passport_tween.start();
+
+					// Enable input and allow for dragging of passport
+					passport.inputEnabled = true;
+					passport.input.enableDrag();
+					passport.events.onDragStart.add(onDragStart, this);
+					passport.events.onDragStop.add(onDragStop, this);
+					game.input.mouse.capture = true;
+					function onDragStart(sprite, pointer) {
+						//result = "Dragging " + sprite.key;
+					}
+					function onDragStop(sprite, pointer) {
+						if (pointer.y > 600) {
+							sprite.input.enabled = false;
+							sprite.sendToBack();
+						}
 					}
 				}
-			}
+			});
+		};
 
-		}
+	loop();
+
 	},
-
-
-//	update: function(){
-//
-//	},
 
 }
--- splash.js
+++ splash.js
@@ -1,16 +1,18 @@
 var splashState = {
 
 	preload: function() {
-		game.load.image('title', 'assets/phaser.png');
+		game.load.image('title', 'graphics/title.png');
 	},
 
 	create: function() {
 
 		// WELCOME PAGE
-		var title = game.add.image(309, 100, 'title');
-		var text;
-		var style = { font: "16px Arial", fill: "#ff0044", align: "center" };
-		text = game.add.text(309,500, "Cliquez sur le bouton gauche de la souris pour commencer", style);
+		var title = game.add.image(283, 150, 'title');
+		var style = { font: "16px Courier", fill: "#ff0044", align: "center" };
+		var style2 = { font: "12px Courier", fill: "#ff0044", align: "center" };
+		var text1 = game.add.text(110, 350, "Décidez du sort de ces pigeons qui, de toutes façons, n'en feront qu'à leur tête", style);		
+		var text2 = game.add.text(380, 450, "Le jeu se joue uniquement à la souris", style2);
+		var text3 = game.add.text(290, 480, "Cliquez sur le bouton gauche de la souris pour commencer", style2);
 		//text.anchor.set(0.9);
 
 		// CALLING THE PLAY STATE WHEN PUSHING A KEY ON KEYBOARD
